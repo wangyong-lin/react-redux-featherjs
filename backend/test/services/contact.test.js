00b22cc34ba1cbe7e6afdef5aaed611eda192265
wangyong-lin <linwangyong91@gmail.com>
2018-11-06 02:07:37 +0800

    Commit react-redux-feathersjs

---
+++ backend/test/services/contact.test.js
@@ -0,0 +1,12 @@
+// backend/test/services/contact.test.js
+
+const assert = require('assert');
+const app = require('../../src/app');
+
+describe('\'contact\' service', () => {
+  it('registered the service', () => {
+    const service = app.service('contacts'); // change contact to contacts
+
+    assert.ok(service, 'Registered the service');
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+// src/App.js
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import ContactListPage from './pages/contact-list-page';
+import ContactFormPage from './pages/contact-form-page';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+        <div className="ui two item menu">
+          <NavLink className="item" activeClassName="active" exact to="/">
+            Contacts List
+          </NavLink>
+          <NavLink className="item" activeClassName="active" exact to="/contacts/new">
+            Add Contact
+          </NavLink>
+        </div>
+        <Route exact path="/" component={ContactListPage}/>
+        <Route path="/contacts/new" component={ContactFormPage}/>
+        <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
+      </Container>
     );
   }
 }
